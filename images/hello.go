6c51c7af9efcd4c4977dc06729393a96610ccc47
airnan <hernantorrisi@gmail.com>
2022-01-10 10:27:14 -0500

    go test

--- images/hello.go
+++ images/hello.go
@@ -1,5 +1,22 @@
 package main
 import "fmt"
+import "os"
+
 func main() {
-    fmt.Println("hello go world")
+    fmt.Println("hello go start")
+		bytesTest, err := os.ReadFile("./test.txt");
+		if err != nil {
+			fmt.Println(err);
+		} else {
+			fileText := string(bytesTest[:]); // fileText is "Hello World!"
+    	fmt.Println(fileText);
+		}
+		jsTest, errJs := os.ReadFile("./index.js");
+		if errJs != nil {
+			fmt.Println(errJs);
+		} else {
+			fileJSText := string(jsTest[:]); // fileText is "Hello World!"
+    	fmt.Println(fileJSText);
+		}
+    fmt.Println("hello go end")
 }
