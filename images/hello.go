bb41e4a9f5b0e9ec6523d23b0a08af157a7b9370
airnan <hernantorrisi@gmail.com>
2022-01-11 13:44:48 -0500

    go test

--- images/hello.go
+++ images/hello.go
@@ -1,6 +1,24 @@
 package main
 import "fmt"
 import "os"
+import "os/exec"
+import "bytes"
+
+func execCommand(command string, args []string) {
+	fmt.Println("==============");
+	fmt.Println("execCommand");
+	cmd := exec.Command(command, args...);
+	var out bytes.Buffer;
+	var stderr bytes.Buffer;
+	cmd.Stdout = &out;
+	cmd.Stderr = &stderr;
+	err := cmd.Run();
+	if err != nil {
+		fmt.Println(fmt.Sprint(err) + ": " + stderr.String());
+		return;
+	}
+	fmt.Println("Result: " + out.String());
+}
 
 func main() {
     fmt.Println("hello go start")
@@ -11,16 +29,18 @@ func main() {
 			fileText := string(bytesTest[:]); // fileText is "Hello World!"
     	fmt.Println(fileText);
 		}
-		jsTest, errJs := os.ReadFile("./index.js");
-		if errJs != nil {
-			fmt.Println(errJs);
-		} else {
-			fileJSText := string(jsTest[:]); // fileText is "Hello World!"
-    	fmt.Println(fileJSText);
-		}
+		// jsTest, errJs := os.ReadFile("./index.js");
+		// if errJs != nil {
+		// 	fmt.Println(errJs);
+		// } else {
+		// 	fileJSText := string(jsTest[:]); // fileText is "Hello World!"
+    // 	fmt.Println(fileJSText);
+		// }
 		////
-		cmd := exec.Command("goldctl")
-		args := []string{"auth", "--work-dir", "./tmp"}
-		cmd := exec.Command("goldctl", args...)
+		execCommand("goldctl", []string{""});
+		execCommand("goldctl", []string{"auth", "--work-dir", "./tmp"});
+		// cmd := exec.Command("goldctl")
+		// args := []string{"auth", "--work-dir", "./tmp"}
+		// cmd2 := exec.Command("goldctl", args...)
     fmt.Println("hello go end")
 }
