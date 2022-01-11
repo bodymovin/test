9aeea2c005f26d746aeeba031bcae337ef8067c9
airnan <hernantorrisi@gmail.com>
2022-01-11 13:53:47 -0500

    go test

--- images/index.js
+++ images/index.js
@@ -1,6 +1,19 @@
-const fs = require('fs');
+// const fs = require('fs');
+const { exec } = require('child_process');
 
-fs.writeFile('./test.txt', 'testing', (err) => {
-  console.log('written');
-  console.log(err);
+// fs.writeFile('./test.txt', 'testing', (err) => {
+//   console.log('written');
+//   console.log(err);
+// });
+
+exec('goldctl', (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
 });
