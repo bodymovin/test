d3923552ab119f68575838ceacdb46447a4df578
airnan <hernantorrisi@gmail.com>
2022-02-08 17:35:34 -0500

    updated yaml

--- images/index.js
+++ images/index.js
@@ -9,11 +9,11 @@ const promiseExec = (command) => (
         reject(error);
         return;
       }
-      if (stderr) {
-        reject(stderr);
+      if (stdout) {
+        resolve(stdout);
         return;
       }
-      resolve(stdout);
+      reject(stderr);
     });
   })
 );
@@ -30,7 +30,6 @@ const promiseWrite = (file, content) => (
   })
 );
 
-
 const callGold = async () => {
   try {
     const result = await promiseExec('goldctl auth --work-dir ./tmp --service-account ./secret.json');
@@ -38,19 +37,36 @@ const callGold = async () => {
   } catch (error) {
     console.log(error);
   }
-  // try {
-  //   const result = await promiseExec('goldctl whoami --work-dir ./tmp --instance lottie-animation-community');
-  //   console.log(result);
-  // } catch (error) {
-  //   console.log(error);
-  // }
+
   try {
-    const githubCommit = process.env.GITHUB_SHA || 'abcd';
+    const githubCommit = 'c31c67181b079b0f596dcc13d82c269096a19194' || process.env.GITHUB_SHA;
+    // const githubCommit = process.env.GITHUB_SHA || '0d36a81a2c37216f5c2a3120af92ade5bce032ea';
     console.log('githubCommit', githubCommit);
-    const result = await promiseExec(`goldctl imgtest init --work-dir ./tmp --commit_id ${githubCommit} --keys-file ./keys.json --instance lottie-animation-community`);
+    const result = await promiseExec(`goldctl imgtest init --work-dir ./tmp --commit ${githubCommit} --keys-file ./keys.json --instance lottie-animation-community --bucket lottie-animation-community-tests`);
     console.log(result);
   } catch (error) {
-    console.log(error);
+    // console.log(error);
+  }
+
+  try {
+    const result = await promiseExec('goldctl imgtest add --work-dir ./tmp --test-name "rectangle" --png-file "./rectangleAnimated.png"');
+    console.log(result);
+  } catch (error) {
+    // console.log('step1', error);
+  }
+
+  try {
+    const result = await promiseExec('goldctl imgtest add --work-dir ./tmp --test-name "image" --png-file "./image.png"');
+    console.log(result);
+  } catch (error) {
+    // console.log('step2', error);
+  }
+
+  try {
+    const result = await promiseExec('goldctl imgtest finalize --work-dir ./tmp');
+    console.log(result);
+  } catch (error) {
+    // console.log('step3', error);
   }
 };
 
