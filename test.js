4419f6a23946670b514af9fd481378560e6f9da9
airnan <hernantorrisi@gmail.com>
2022-04-05 18:36:24 -0400

    test

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 fs.readdir('./', (err, files) => {
   files.forEach(file => {
@@ -6,3 +8,15 @@ fs.readdir('./', (err, files) => {
   });
 });
 console.log('TESTING')
+
+
+try {
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+  console.log('=========== REST ======');
+  console.log(github.rest);
+} catch (error) {
+  core.setFailed(error.message);
+}
