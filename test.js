32efd09634238da5dbbd3f32dc3696ca6419eb31
airnan <hernantorrisi@gmail.com>
2022-04-05 20:18:38 -0400

    test

--- test.js
+++ test.js
@@ -1,31 +1,41 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const fs = require('fs');
+const {readdir} = require('fs/promises');
 const path = require('path');
 
+
 console.log('TESTING 1')
-const data = fs.readdir(
-  path.join(process.env.GITHUB_WORKSPACE, 'data')
-)
-
-console.log('data')
-console.log(data)
-
-
-try {
-  console.log('TESTING 5')
-  // console.log(core)
-  const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-  const octokit = github.getOctokit(GITHUB_TOKEN);
-  // Issue title: github.event.issue.title
-  // Issue body: github.event.issue.body
-
-  console.log('=========== REST ======');
-  // console.log(github);
-  // console.log(octokit);
-  // const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`);
-
-} catch (error) {
-  core.setFailed(error.message);
+async function readFiles() {
+  try {
+    const data = await readdir(
+      path.join(process.env.GITHUB_WORKSPACE, 'data')
+    )
+  } catch(error) {
+    console.log('readFiles error');
+    console.log(error);
+  }
 }
+
+async function run() {
+  await readFiles()
+  console.log('RUN ENDED')
+  try {
+    console.log('TESTING 5')
+    // console.log(core)
+    const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+    const octokit = github.getOctokit(GITHUB_TOKEN);
+    // Issue title: github.event.issue.title
+    // Issue body: github.event.issue.body
+  
+    console.log('=========== REST ======');
+    // console.log(github);
+    // console.log(octokit);
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
+  
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run()
