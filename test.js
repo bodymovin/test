24fcd7ce73d0eeb9cfe9e8903d5a823b39468191
airnan <hernantorrisi@gmail.com>
2022-04-05 22:47:51 -0400

    test

--- test.js
+++ test.js
@@ -3,14 +3,16 @@ const github = require('@actions/github');
 const {readdir, readFile, writeFile} = require('fs/promises');
 const path = require('path');
 
-
-console.log('TESTING 1')
-
 const editFile = async (filePath, player, version) => {
   const file = await readFile(filePath, 'utf-8');
   const fileData = JSON.parse(file);
-  fileData.stats[player][version] = 'y'
-  const updateFileString = JSON.stringify(fileData, null, 2)
+  if (!fileData.stats[player]) {
+    throw new Error('Player does not exist');
+  }
+  const player = fileData.stats[player];
+  const objKeys = Object.keys(player)
+  fileData.stats[player][version] = player[objKeys[objKeys.length - 1]];
+  const updateFileString = JSON.stringify(fileData, null, 2);
   return await writeFile(filePath, updateFileString);
 }
 
@@ -32,20 +34,29 @@ async function updateFiles(player, version) {
   }
 }
 
+const getPlayerData = async(issueBody) => {
+  console.log(issueBody)
+  return {
+    version: '5.9.13',
+    player: 'lottie-web',
+  }
+}
+
+const getBranchName = async(playerData) => {
+  return `${playerData.player}__${playerData.version}`
+}
+
 async function run() {
-  await updateFiles('lottie-web', '5.9.13')
-  console.log('RUN ENDED')
   try {
-    console.log('TESTING 5')
-    // console.log(core)
-    core.setOutput("branch_name", "branch_name_job2");
-    const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-    const octokit = github.getOctokit(GITHUB_TOKEN);
-    // Issue title: github.event.issue.title
-    // Issue body: github.event.issue.body
-    console.log('TESTING 7')
-  
-  
+    if (github.event.issue.title.toLowerCase() !== 'new version') {
+      // return;
+    }
+    const playerData = await getPlayerData(ithub.event.issue.body);
+    const branchName = getBranchName(playerData);
+    core.setOutput("branch_name", branchName);
+
+    await updateFiles(playerData.player, playerData.version);
+    console.log('RUN ENDED');
   } catch (error) {
     core.setFailed(error.message);
   }
