bec7e579cf57c96284414d9db5852d02c2ba9222
airnan <hernantorrisi@gmail.com>
2022-10-05 08:57:27 -0400

    yaml

--- new_version/run_tests.ts
+++ new_version/run_tests.ts
@@ -1,29 +1,32 @@
-import * as core from '@actions/core';
 import * as github from '@actions/github';
 import yargs from 'yargs';
+
+function getTestPayload(player) {
+  if(player === 'lottie-web' || player === 'skottie') {
+    return {
+      player,
+    }
+  }
+}
 
 async function run() {
   try {
     console.log('Running test');
-    const player = core.getInput('player');
-    console.log('playerplayer', player);
-    console.log('=== process.arg ===');
-    console.log(process.argv);
     // const token = 'a';
     const argv = yargs(process.argv).argv
-    console.log('==== argv ====')
-    console.log(argv.player)
     const secretToken = process.env.GITHUB_TOKEN || 'none secret';
-    console.log('secretToken', secretToken.length);
     const octokit = github.getOctokit(secretToken);
-    console.log(typeof octokit);
-    const result = await octokit.rest.repos.createDispatchEvent({
-      owner: 'bodymovin',
-      repo: 'test',
-      event_type: 'trigger-test',
-      client_payload: {},
-    });
-    console.log('result', result);
+    const payload = getTestPayload(argv.player);
+    console.log('payload', payload)
+    if (payload) {
+      const result = await octokit.rest.repos.createDispatchEvent({
+        owner: 'bodymovin',
+        repo: 'test',
+        event_type: 'trigger-test',
+        client_payload: {},
+      });
+      console.log('result', result);
+    }
   } catch (error) {
     console.log('RUN ERROR: ', error);
   }
