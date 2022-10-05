5c8a7d46e05646b359f26b9f9775a3eb70484a7a
airnan <hernantorrisi@gmail.com>
2022-10-05 08:21:43 -0400

    run_test action

---
+++ new_version/run_tests.ts
@@ -0,0 +1,30 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+async function run() {
+  try {
+    console.log('Running test');
+    const player = core.getInput('player');
+    console.log('playerplayer', player);
+    // const token = 'a';
+    const secretToken = process.env.GITHUB_TOKEN || 'none secret';
+    console.log('secretToken', secretToken.length);
+    const octokit = github.getOctokit(secretToken);
+    console.log(typeof octokit);
+    const result = await octokit.rest.repos.createDispatchEvent({
+      owner: 'bodymovin',
+      repo: 'test',
+      event_type: 'trigger-test',
+      client_payload: {},
+    });
+    console.log('result', result);
+  } catch (error) {
+    console.log('RUN ERROR: ', error);
+  }
+}
+
+try {
+  run();
+} catch (error) {
+  console.log('FAILED', error);
+}
